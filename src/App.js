70f704a429b62be47e7e45dbd02f3036536f5461
dlicon2 <dlicon2@miners.utep.edu>
2023-01-23 13:13:01 -0700

    s

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Hellooo Mine Benders
+          Hellooo Mine Bendersss
         </p>
         <a
           className="App-link"
